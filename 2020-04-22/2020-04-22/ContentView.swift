885ba00b526ddff767431ada2700509f8d428f41
Jeff Boek <jeff@jeffboek.com>
2020-04-22 18:49:53 -0700

    Adds search field and emoji data type and emoji data

--- 2020-04-22/2020-04-22/ContentView.swift
+++ 2020-04-22/2020-04-22/ContentView.swift
@@ -8,9 +8,24 @@
 
 import SwiftUI
 
+struct Emoji: Identifiable, CustomStringConvertible {
+    var id: String { return name }
+    var description: String { return "\(emoji)" }
+    
+    var name: String
+    var emoji: Character
+}
+
+var emoji = [
+    Emoji(name: "", emoji: <#T##Character#>)
+]
+
 struct ContentView: View {
+    @State var searchText = ""
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            TextField("Search", text: $searchText)
+        }
     }
 }
 
