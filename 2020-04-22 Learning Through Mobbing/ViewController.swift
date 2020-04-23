0babbbcbc2ce417725f3bdf73654010371381e19
Sam Foulger <samfoulger@gmail.com>
2020-04-22 18:40:52 -0700

    Decscript

--- 2020-04-22 Learning Through Mobbing/ViewController.swift
+++ 2020-04-22 Learning Through Mobbing/ViewController.swift
@@ -6,10 +6,23 @@
 //  Copyright © 2020 Sam Foulger. All rights reserved.
 //
 
+//Project description
+//Emoji Picker with Search
+//
+//List of all available emoji
+//Ability to search based on their name
+//Keyboard hidden on selection (gives copy to clipboard dialogue)
+//Ability to dismiss keyboard
+
+
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
